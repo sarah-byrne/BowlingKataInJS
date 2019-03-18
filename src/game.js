eb164347afa7e035f496404f6bdaf7ce4029062c
Steve Kahn <steve@kahn.org.uk>
2019-03-18 10:54:59 +0000

    Removed console logs

--- src/game.js
+++ src/game.js
@@ -13,15 +13,9 @@ Game.prototype.score = function() {
   for (i = 0; i < this.rolls.length; i+=2){
     if (this.rolls[i] + this.rolls[i+1] == 10){
       console.log(i)
-
-      console.log("roll 1 ="+ this.rolls[i])
-      console.log("roll 2 =" + this.rolls[i+1])
-      console.log("roll 3 =" + this.rolls[i+2])
       this.total += this.rolls[i+2]
-      console.log("total = "+ this.total)
     }
     this.total += this.rolls[i] + this.rolls[i+1]
-    console.log("total = "+ this.total)
   }
     return this.total
 }
