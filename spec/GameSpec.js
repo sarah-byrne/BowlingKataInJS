3681f22a220e780598db8a488c4011afb6872a39
Steve Kahn <steve@kahn.org.uk>
2019-03-18 10:54:11 +0000

    Added spare functionality

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,9 +1,28 @@
 describe('game', function() {
+  beforeEach(function(){
+    g = new Game();
+  })
+
+  rollMany = function(n, pins){
+    for (i = 0; i < n; i++) {
+        g.roll(pins)
+    }
+  }
     it('gutter game', function() {
-        g = new Game();
-        for (i = 0; i < 20; i++) {
-            g.roll(0)
-        }
+        rollMany(20, 0)
         expect(g.score()).toEqual(0);
+    })
+
+    it('1 pin per roll', function() {
+      rollMany(20, 1)
+      expect(g.score()).toEqual(20)
+    })
+
+    it('Has a score 16 after a spare and a 3', function(){
+      g.roll(5)
+      g.roll(5)
+      g.roll(3)
+      rollMany(17,0)
+      expect(g.score()).toEqual(16)
     })
 });
--- src/game.js
+++ src/game.js
@@ -1,11 +1,27 @@
 function Game() {
-    console.log("game")
+  this.total = 0
+  this.rolls = []
+  this.currentroll = 0
 };
 
 Game.prototype.roll = function(pins) {
-
+  // this.total += pins
+  this.rolls.push(pins)
 }
 
 Game.prototype.score = function() {
-    return 0
+  for (i = 0; i < this.rolls.length; i+=2){
+    if (this.rolls[i] + this.rolls[i+1] == 10){
+      console.log(i)
+
+      console.log("roll 1 ="+ this.rolls[i])
+      console.log("roll 2 =" + this.rolls[i+1])
+      console.log("roll 3 =" + this.rolls[i+2])
+      this.total += this.rolls[i+2]
+      console.log("total = "+ this.total)
+    }
+    this.total += this.rolls[i] + this.rolls[i+1]
+    console.log("total = "+ this.total)
+  }
+    return this.total
 }
